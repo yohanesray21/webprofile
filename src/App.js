fc20b8145e5f0886dd0ab943ca06a020d02214d0
Yohanesray <yohanesray@pop-os.localdomain>
2021-07-21 23:53:49 +0700

    Update Page one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Avatar,
   Box,
@@ -7,60 +7,77 @@ import {
   HStack,
   Icon,
   IconButton,
+  Image,
   Text,
   VStack,
   WrapItem,
 } from "@chakra-ui/react";
 
-import { SearchIcon } from "@chakra-ui/icons";
-import { VscHome } from "react-icons/vsc";
+import { BsHouseFill, BsPersonBoundingBox } from "react-icons/bs";
+import {
+  FaCode,
+  FaComments,
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+  FaStream,
+  FaTwitter,
+} from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
 
-function App() {
+import Annes from "./images/Annes.jpeg";
+import Logo from "./images/Logo.png";
+import Logov2 from "./images/Logov2.png";
+import ThemeButton from "./ThemeButton";
+
+function App(props) {
+  const [logo, setLogo] = useState(true);
+
   return (
     <>
       {/* <Container border="5px" maxW="full" bgColor="blue.200" p={0} m={0}> */}
       <Flex>
         <Box
           borderRight="1px"
-          bgColor="orange.100"
           p={10}
           h="100vh"
           minHeight="100vh"
           position="fixed"
         >
           <Flex direction="column" w="190px">
-            <Box>
-              <Text>Yohanes.</Text>
+            <Image boxSize="150px" src={logo ? Logov2 : Logo} alt="" />
+            <Box onClick={() => setLogo((logo) => !logo)}>
+              <ThemeButton />
             </Box>
-            <VStack pt={10} w="70px" h="560px" mt={50}>
-              <HStack px={5} m={0} cursor="pointer">
-                <Icon color="teal" fontSize="xl" as={VscHome}></Icon>
-                <Text fontWeight="bold" pt={1} pl={2}>
+            <VStack pt={20} w="70px" h="560px" mt={50}>
+              <HStack px={5} pb={2} m={0} cursor="pointer">
+                <Icon fontSize="xl" as={BsHouseFill}></Icon>
+                <Text fontSize="lg" fontWeight="bold" pt={1} pl={2}>
                   Home
                 </Text>
               </HStack>
-              <HStack px={5} m={0} cursor="pointer">
-                <Icon color="teal" fontSize="xl" as={VscHome}></Icon>
-                <Text fontWeight="bold" pt={1} pl={2}>
-                  Home
+              <HStack px={5} pb={2} m={0} cursor="pointer">
+                <Icon fontSize="xl" as={BsPersonBoundingBox}></Icon>
+                <Text fontSize="lg" fontWeight="bold" pt={1} pl={2}>
+                  About
                 </Text>
               </HStack>
-              <HStack px={5} m={0} cursor="pointer">
-                <Icon color="teal" fontSize="xl" as={VscHome}></Icon>
-                <Text fontWeight="bold" pt={1} pl={2}>
-                  Home
+              <HStack px={5} pb={2} m={0} cursor="pointer">
+                <Icon fontSize="xl" as={FaCode}></Icon>
+                <Text fontSize="lg" fontWeight="bold" pt={1} pl={2}>
+                  Skills
                 </Text>
               </HStack>
-              <HStack px={5} m={0} cursor="pointer">
-                <Icon color="teal" fontSize="xl" as={VscHome}></Icon>
-                <Text fontWeight="bold" pt={1} pl={2}>
-                  Home
+              <HStack pl={4} pb={2} m={0} cursor="pointer">
+                <Icon fontSize="xl" as={FaStream}></Icon>
+                <Text fontSize="lg" fontWeight="bold" pt={1} pl={2}>
+                  Project
                 </Text>
               </HStack>
-              <HStack px={5} m={0} cursor="pointer">
-                <Icon color="teal" fontSize="xl" as={VscHome}></Icon>
-                <Text fontWeight="bold" pt={1} pl={2}>
-                  Home
+              <HStack pl={5} pb={2} m={0} cursor="pointer">
+                <Icon fontSize="xl" as={FaComments}></Icon>
+                <Text fontSize="lg" fontWeight="bold" pt={1} pl={2}>
+                  Contact
                 </Text>
               </HStack>
             </VStack>
@@ -70,7 +87,16 @@ function App() {
           </Flex>
         </Box>
       </Flex>
-      <Flex w="auto" h="100vh" ml={270}>
+      <Flex
+        w="auto"
+        h="100vh"
+        ml={270}
+        // bgGradient={[
+        //   "linear(to-tr, teal.300,yellow.400)",
+        //   "linear(to-t, blue.200, teal.500)",
+        //   "linear(to-b, orange.100, gray.300)",
+        // ]}
+      >
         <Box className="container" w="100%" mx={40} my="auto" px={15}>
           <VStack
             justifyContent="center"
@@ -82,18 +108,21 @@ function App() {
             zIndex={1}
           >
             <WrapItem>
-              <Avatar
-                size="xl"
-                name="Christian Nwamba"
-                src="https://bit.ly/code-beast"
-              />{" "}
+              <Avatar size="xl" name="Christian Nwamba" src={Annes} />
             </WrapItem>
             <Heading as="h4" size="md">
               Yohanes Ray Febriyanto Silitonga
             </Heading>
             <Text fontSize="md">I'm a Front-End Developer</Text>
-            <HStack spacing={4}>
-              <IconButton aria-label="Search database" icon={<SearchIcon />} />
+            <HStack spacing={2}>
+              <IconButton
+                aria-label="Search database"
+                icon={<FaLinkedinIn />}
+              />
+              <IconButton aria-label="Search database" icon={<FaGithub />} />
+              <IconButton aria-label="Search database" icon={<IoIosMail />} />
+              <IconButton aria-label="Search database" icon={<FaTwitter />} />
+              <IconButton aria-label="Search database" icon={<FaInstagram />} />
             </HStack>
           </VStack>
         </Box>
---
+++ src/ThemeButton.js
@@ -0,0 +1,22 @@
+import { Button, useColorMode } from "@chakra-ui/react";
+import React from "react";
+
+function ThemeButton() {
+  const { toggleColorMode } = useColorMode();
+
+  //   const bg = useColorModeValue("red.500", "red.200");
+  //   const color = useColorModeValue("white", "gray.800");
+
+  return (
+    <>
+      {/* <Box mb={4} bg={bg} color={color}>
+        This box's style will change based on the color mode.
+      </Box> */}
+      <Button size="sm" onClick={toggleColorMode}>
+        Toggle Mode
+      </Button>
+    </>
+  );
+}
+
+export default ThemeButton;
